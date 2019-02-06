9c7ae2aaadeb85bc4c789c241034836127ba471b
Lucas Belfanti <lhbelfanti@gmail.com>
2019-02-06 20:48:25 -0300

    Removed config.json from the necessaries parameters to run the program

--- client/src/config/config.py
+++ client/src/config/config.py
@@ -6,8 +6,8 @@ class Configuration:
     config = {}
 
     @staticmethod
-    def load(file_name):
-        with open(file_name) as config_file:
+    def load():
+        with open("config.json") as config_file:
             Logger.info("Loading configuration file.")
             Configuration.config = json.load(config_file)
             Logger.info("Configuration loaded.")
--- client/src/start.py
+++ client/src/start.py
@@ -10,14 +10,13 @@ from selenium.webdriver.chrome.options import Options
 Logger.load()
 args = sys.argv
 config_file = ""
-if len(args) != 4:
+if len(args) != 3:
     Logger.error("Twitter's user and password needed")
-    Logger.error("USAGE: python3 start.py <TWITTER_USER> <TWITTER_PASSWORD> <CONFIG_FILE>")
+    Logger.error("USAGE: python3 start.py <TWITTER_USER> <TWITTER_PASSWORD>")
     exit(1)
 else:
     constants.USERNAME = args[1]
     constants.PASSWORD = args[2]
-    config_file = args[3]
 
 
 # Load the Chrome webdriver
@@ -31,7 +30,7 @@ chrome_driver = "chromedriver"
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
 
 # Load config
-Configuration.load(config_file)
+Configuration.load()
 
 # Start the app
 twitter = twitter_news.TwitterNews(driver)
